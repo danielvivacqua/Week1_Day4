ca4a7b802a6259af8a9fa7d9b261874029a2bac9
danielvivacqua <drvivacqua@hotmail.com>
2017-09-28 15:42:30 -0400

    Final in class exercises

--- Week1_Day4/Program.cs
+++ Week1_Day4/Program.cs
@@ -10,6 +10,29 @@ namespace Week1_Day4
     {
         static void Main(string[] args)
         {
+            //A string is a sequence of characters
+            //inside "double quotes"
+
+            //Console.WriteLine("This is a tab \t.");
+            //Console.WriteLine("This is a \"quote\".");
+            //Console.WriteLine("This is a backslash \\");
+            //Console.WriteLine("This is a \n new line.");
+            //Console.WriteLine("And this is a bell \a");
+
+            //Console.WriteLine("My name is {0} and my dog is {1}.", firstName, dogName);
+
+            //string bestFriends = firstName + " " + dogName;
+
+            string firstName = "Daniel";
+            string dogName = "Gus";
+
+            string bestFriends = string.Concat(firstName, dogName);
+            Console.WriteLine(bestFriends);
+
+            int danielLength = firstName.Length;
+
+            Console.WriteLine(danielLength);
+            Console.WriteLine(bestFriends.Length);
         }
     }
 }
